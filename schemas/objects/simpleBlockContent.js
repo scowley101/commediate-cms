099c6e8b1cc3ab88fd5c1436700aca395a484210
Sam Cowley <sfcowley@gmail.com>
2022-01-27 17:34:17 +0000

    simpleBlockCOntent updated

--- schemas/objects/simpleBlockContent.js
+++ schemas/objects/simpleBlockContent.js
@@ -6,13 +6,26 @@ export default {
       {
         title: 'Block',
         type: 'block',
-        styles: [],
+        styles: [
+            {title: 'H1', value: 'h1'},
+           {title: 'H2', value: 'h2'},
+           {title: 'H3', value: 'h3'},
+           {title: 'H4', value: 'h4'},
+           {title: 'H5', value: 'h5'},
+           {title: 'H6', value: 'h6'},
+           {title: 'H7', value: 'h7'},
+           {title: 'Text Base', value: 'text-base'},
+           {title: 'Text Large', value: 'text-lg'},
+           {title: 'Text XL', value: 'text-xl'},
+           {title: 'Text 2XL', value: 'text-2xl'},
+        ],
         marks: {
           decorators: [
             {title: 'Strong', value: 'strong'},
             {title: 'Emphasis', value: 'em'},
             {title: 'Italic', value: 'italic'},
-            {title: 'Code', value: 'code'}
+            {title: 'Code', value: 'code'},
+            {title: 'Underline', value: 'underline'}
           ],
           annotations: [
             {
