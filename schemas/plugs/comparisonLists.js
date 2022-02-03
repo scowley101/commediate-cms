2806465aff1020c3b981fd0e2e3c6009a3c4e0b7
Sam Cowley <sfcowley@gmail.com>
2022-02-03 13:19:06 +0000

    Added comparisonList

---
+++ schemas/plugs/comparisonLists.js
@@ -0,0 +1,74 @@
+import {FiColumns} from "react-icons/fi";
+
+
+export default {
+    type: 'object',
+    name: 'comparisonList',
+    title: 'Comparison Lists',
+    icon: FiColumns,
+    description: '',
+    fields: [
+        {
+            title: 'Heading',
+            name: 'heading',
+            type: 'simpleBlockContent',
+            title: 'Heading'
+          },
+          {
+            title: 'Tagline',
+            name: 'tagline',
+            type: 'simpleBlockContent'
+          },
+          {
+            title: 'Pro Icon',
+            name: 'proIcon',
+            type: 'reference',
+            to: [
+              {type: 'icons'},
+            ]
+          },
+          {
+            title: 'Pros title',
+            name: 'prosTitle',
+            type: 'simpleBlockContent'
+          },
+          {
+            title: 'Pros list',
+            name: 'prosList',
+            type: 'simpleBlockContent'
+          },
+          {
+            title: 'Con Icon',
+            name: 'conIcon',
+            type: 'reference',
+            to: [
+              {type: 'icons'},
+            ]
+          },
+          {
+            title: 'Cons title',
+            name: 'consTitle',
+            type: 'simpleBlockContent'
+          },
+          {
+            title: 'Cons list',
+            name: 'consList',
+            type: 'simpleBlockContent'
+          },
+          {
+            name: 'cta',
+            type: 'cta'
+          },
+    ],
+    preview: {
+      select: {
+        title: 'title'
+      },
+      prepare({title}) {
+        return {
+          title: 'Two Column List',
+          subtitle: title
+        }
+      }
+    }
+  }
